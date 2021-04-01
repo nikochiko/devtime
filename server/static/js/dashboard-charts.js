16d836b346a4f1de36d0d8af6a250e9ff3177a65
nikochiko <kaustubh299792458@gmail.com>
2021-04-01 21:44:14 +0530

    Fix colors of pie chart

--- server/static/js/dashboard-charts.js
+++ server/static/js/dashboard-charts.js
@@ -2,6 +2,18 @@ Number.prototype.comma_formatter = function() {
     return this.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
 
+let compareDatesInDDMMYY = function(a, b) {
+    let [dayA, monthA, yearA] = a.split('-');
+    let [dayB, monthB, yearB] = b.split('-');
+
+    // -1 if a < b, 1 if a > b, and 0 if a == b
+    if (yearA === yearB) {
+        return monthA === monthB ? Number(dayA) - Number(dayB) : Number(monthA) - Number(monthB);
+    } else {
+        return Number(yearA) - Number(yearB);
+    }
+};
+
 let chartData = function(authToken, chartType="line"){
     return {
         date: 'today',
@@ -83,22 +95,22 @@ let chartData = function(authToken, chartType="line"){
 
             let ctx = document.getElementById("daywiseChart").getContext("2d");
 
+            let sortedDates = Object.keys(this.data);
+            sortedDates.sort(compareDatesInDDMMYY);
+            
             let dataPoints = [];
-
-            if (this.data) {
-                for (const [key, value] of Object.entries(this.data)) {
-                    if (this.selectedLanguage) {
-                        dataPoints.push(value.languages[this.selectedLanguage]);
-                    } else {
-                        dataPoints.push(value.total);
-                    }
+            for (const date of sortedDates) {
+                if (this.selectedLanguage) {
+                    dataPoints.push(this.data[date].languages[this.selectedLanguage]);
+                } else {
+                    dataPoints.push(this.data[date].total);
                 }
             }
 
             let chart = new Chart(ctx, {
                 type: chartType,
                 data: {
-                    labels: Object.keys(this.data),
+                    labels: sortedDates,
                     datasets: [
                         {
                             label: "Time (in minutes)",
@@ -128,6 +140,112 @@ let chartData = function(authToken, chartType="line"){
     }
 };
 
+function colorChart(ctx, chart) {
+    let palette = ["cool", "warm", "neon"][Math.floor(Math.random() * 3)];
+
+    /*Gradients
+      The keys are percentage and the values are the color in a rgba format.
+      You can have as many "color stops" (%) as you like.
+      0% and 100% is not optional.*/
+    var gradient;
+    switch (palette) {
+        case 'cool':
+            gradient = {
+                0: [255, 255, 255, 1],
+                20: [220, 237, 200, 1],
+                45: [66, 179, 213, 1],
+                65: [26, 39, 62, 1],
+                100: [0, 0, 0, 1]
+            };
+            break;
+        case 'warm':
+            gradient = {
+                0: [255, 255, 255, 1],
+                20: [254, 235, 101, 1],
+                45: [228, 82, 27, 1],
+                65: [77, 52, 47, 1],
+                100: [0, 0, 0, 1]
+            };
+            break;
+        case 'neon':
+            gradient = {
+                0: [255, 255, 255, 1],
+                20: [255, 236, 179, 1],
+                45: [232, 82, 133, 1],
+                65: [106, 27, 154, 1],
+                100: [0, 0, 0, 1]
+            };
+            break;
+    }
+
+    //Get a sorted array of the gradient keys
+    var gradientKeys = Object.keys(gradient);
+    gradientKeys.sort(function(a, b) {
+        return +a - +b;
+    });
+
+    //Find datasets and length
+    var chartType = chart.config.type;
+    switch (chartType) {
+        case "pie":
+        case "doughnut":
+            var datasets = chart.config.data.datasets[0];
+            var setsCount = datasets.data.length;
+            break;
+        case "bar":
+        case "line":
+            var datasets = chart.config.data.datasets;
+            var setsCount = datasets.length;
+            break;
+    }
+
+    //Calculate colors
+    var chartColors = [];
+    for (i = 0; i < setsCount; i++) {
+        var gradientIndex = (i + 1) * (100 / (setsCount + 1)); //Find where to get a color from the gradient
+        for (j = 0; j < gradientKeys.length; j++) {
+            var gradientKey = gradientKeys[j];
+            if (gradientIndex === +gradientKey) { //Exact match with a gradient key - just get that color
+                chartColors[i] = 'rgba(' + gradient[gradientKey].toString() + ')';
+                break;
+            } else if (gradientIndex < +gradientKey) { //It's somewhere between this gradient key and the previous
+                var prevKey = gradientKeys[j - 1];
+                var gradientPartIndex = (gradientIndex - prevKey) / (gradientKey - prevKey); //Calculate where
+                var color = [];
+                for (k = 0; k < 4; k++) { //Loop through Red, Green, Blue and Alpha and calculate the correct color and opacity
+                    color[k] = gradient[prevKey][k] - ((gradient[prevKey][k] - gradient[gradientKey][k]) * gradientPartIndex);
+                    if (k < 3) color[k] = Math.round(color[k]);
+                }
+                chartColors[i] = 'rgba(' + color.toString() + ')';
+                break;
+            }
+        }
+    }
+
+    //Copy colors to the chart
+    for (i = 0; i < setsCount; i++) {
+        switch (chartType) {
+            case "pie":
+            case "doughnut":
+                if (!datasets.backgroundColor) datasets.backgroundColor = [];
+                datasets.backgroundColor[i] = chartColors[i];
+                if (!datasets.borderColor) datasets.borderColor = [];
+                datasets.borderColor[i] = "rgba(255,255,255,1)";
+                break;
+            case "bar":
+                datasets[i].backgroundColor = chartColors[i];
+                datasets[i].borderColor = "rgba(255,255,255,0)";
+                break;
+            case "line":
+                datasets[i].borderColor = chartColors[i];
+                datasets[i].backgroundColor = "rgba(255,255,255,0)";
+                break;
+        }
+    }
+
+    //Update the chart to show the new colors
+    chart.update();
+}
 
 let todaysStats = function(authToken, chartType="pie"){
     return {
@@ -165,9 +283,6 @@ let todaysStats = function(authToken, chartType="pie"){
                     datasets: [
                         {
                             label: "Time (in minutes)",
-                            backgroundColor: "rgba(102, 100, 234, 0.25)",
-                            borderColor: "rgba(102, 126, 200, 1)",
-                            pointBackgroundColor: "rgba(102, 200, 234, 1)",
                             data: Object.values(this.data.languages),
                         },
                     ],
@@ -187,6 +302,8 @@ let todaysStats = function(authToken, chartType="pie"){
                     }
                 }
             });
+
+            colorChart(ctx, chart);
         }
     }
 };
